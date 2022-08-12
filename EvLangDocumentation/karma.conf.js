7dbcd8055dca134e180b8f3bd9dd6cbf0b484c1e
Evryon <fireden70@gmail.com>
2022-08-12 13:42:35 +0200

    Start

--- EvLangDocumentation/karma.conf.js
+++ EvLangDocumentation/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ev-lang-doc'),
+      dir: require('path').join(__dirname, './coverage/ev-lang-documentation'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- EvLangDocumentation/src/app/app.component.spec.ts
+++ EvLangDocumentation/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'EvLangDoc'`, () => {
+  it(`should have as title 'EvLangDocumentation'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('EvLangDoc');
+    expect(app.title).toEqual('EvLangDocumentation');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('EvLangDoc app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('EvLangDocumentation app is running!');
   });
 });
